e633f2923a6492e1e9a68acad4824d5f57adad3b
Danny AJ <linshengte@gmail.com>
2018-02-26 22:45:48 +0800

    Add files via upload

---
+++ microservice-simple-provider-user-trace-elk/src/test/java/om/itmuch/cloud/microservicesimpleproviderusertraceelk/MicroserviceSimpleProviderUserTraceElkApplicationTests.java
@@ -0,0 +1,18 @@
+package om.itmuch.cloud.microservicesimpleproviderusertraceelk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.itmuch.cloud.study.ProviderUserApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ProviderUserApplication.class)
+public class MicroserviceSimpleProviderUserTraceElkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ microservice-simple-provider-user-trace/src/test/java/om/itmuch/cloud/microservicesimpleproviderusertrace/MicroserviceSimpleProviderUserTraceApplicationTests.java
@@ -0,0 +1,16 @@
+package om.itmuch.cloud.microservicesimpleproviderusertrace;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroserviceSimpleProviderUserTraceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
