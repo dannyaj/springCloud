f4e4c70c116608369febea0c51e6d4e42dfe6122
Danny AJ <linshengte@gmail.com>
2018-02-26 22:23:58 +0800

    Add files via upload

---
+++ microservice-consumer-movie-feign-logging/src/main/java/com/itmuch/cloud/study/user/controller/MovieController.java
@@ -0,0 +1,29 @@
+package com.itmuch.cloud.study.user.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.feign.FeignClientsConfiguration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.itmuch.cloud.study.user.entity.User;
+import com.itmuch.cloud.study.user.feign.UserFeignClient;
+
+import feign.Client;
+import feign.Contract;
+import feign.Feign;
+import feign.auth.BasicAuthRequestInterceptor;
+import feign.codec.Decoder;
+import feign.codec.Encoder;
+
+@RestController
+public class MovieController {
+  @Autowired
+  private UserFeignClient userFeignClient;
+
+  @GetMapping("/user/{id}")
+  public User findById(@PathVariable Long id) {
+    return this.userFeignClient.findById(id);
+  }
+}
