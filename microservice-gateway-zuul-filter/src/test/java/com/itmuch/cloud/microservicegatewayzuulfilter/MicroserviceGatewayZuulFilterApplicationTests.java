a13c5ca136585311deb5199e107eefccadb2da7b
Danny AJ <linshengte@gmail.com>
2018-02-26 22:32:52 +0800

    Add files via upload

---
+++ microservice-gateway-zuul-filter/src/test/java/com/itmuch/cloud/microservicegatewayzuulfilter/MicroserviceGatewayZuulFilterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.itmuch.cloud.microservicegatewayzuulfilter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroserviceGatewayZuulFilterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
