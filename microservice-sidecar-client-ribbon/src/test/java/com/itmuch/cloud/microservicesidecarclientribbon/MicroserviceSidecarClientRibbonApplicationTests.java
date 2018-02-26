f5070b33e8d250bfc9d950d755d3a1237acb9071
Danny AJ <linshengte@gmail.com>
2018-02-26 22:41:43 +0800

    Add files via upload

---
+++ microservice-sidecar-client-ribbon/src/test/java/com/itmuch/cloud/microservicesidecarclientribbon/MicroserviceSidecarClientRibbonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.itmuch.cloud.microservicesidecarclientribbon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroserviceSidecarClientRibbonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
