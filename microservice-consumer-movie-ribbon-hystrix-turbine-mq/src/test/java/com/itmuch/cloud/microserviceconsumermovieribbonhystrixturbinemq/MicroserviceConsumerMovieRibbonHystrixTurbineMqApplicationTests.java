b65ebef16f3dbe61f455c2f79b324f1c8d7584ac
Danny AJ <linshengte@gmail.com>
2018-02-26 22:26:30 +0800

    Add files via upload

---
+++ microservice-consumer-movie-ribbon-hystrix-turbine-mq/src/test/java/com/itmuch/cloud/microserviceconsumermovieribbonhystrixturbinemq/MicroserviceConsumerMovieRibbonHystrixTurbineMqApplicationTests.java
@@ -0,0 +1,16 @@
+package com.itmuch.cloud.microserviceconsumermovieribbonhystrixturbinemq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroserviceConsumerMovieRibbonHystrixTurbineMqApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
