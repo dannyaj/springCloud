c6eabfe7ec0e599d40bc0b1ccc5454eee958501b
Danny AJ <linshengte@gmail.com>
2018-02-26 22:17:02 +0800

    Add files via upload

---
+++ microservice-config-server-refresh-cloud-bus/src/test/java/com/itmuch/cloud/microserviceconfigserverrefreshcloudbus/MicroserviceConfigServerRefreshCloudBusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.itmuch.cloud.microserviceconfigserverrefreshcloudbus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroserviceConfigServerRefreshCloudBusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
